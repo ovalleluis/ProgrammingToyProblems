c440c6f9a946ced31b96c73ae18204bfd7262a88
LuisOvalle <ovalle.luis@gmail.com>
2014-09-12 16:18:30 +0200

    Remebering scala for/yields

--- src/module1/RotateMatrix90.scala
+++ src/module1/RotateMatrix90.scala
@@ -4,10 +4,26 @@ import Array._
 
 
 
-class RotateMatrix90 {
+object RotateMatrix90 {
 
-    def weight(n: Int): Array[Int] =  {
-    	null;
+    def matrix(n: Int): Array[Array[Int]] =  {
+    	Array.ofDim[Int](n,n) ;   	
     } 
+    
+    
+    
+    def main(args: Array[String]) {
+      
+    	println( matrix(5).map(_.mkString(" ")).mkString("\n")
+    	       )
+    	var n = 5 
+    	
+    	var matrix = Array();
+    	       
+    	matrix :+ (
+    			for (j <- 1 to n) yield List.tabulate(n)( i => (i + 1) + ((j-1)*n)))
+    	
+  
+    }
   
 }
